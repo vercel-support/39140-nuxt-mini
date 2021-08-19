b21a1cbb9d127613f9f04206fa60fe437f1b6d4b
Costantin Bignoli <cosbgn@gmail.com>
2021-08-19 11:57:32 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+	  Homepage.
+	  <br>
+	  Visiting <nuxt-link :to="`/${sub}.loginhook.com`">{{sub}}.loginhook.com</nuxt-link> should show the message: "Hello user {{sub}}"
+  </div>
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			sub:''
+		}
+	},
+	mounted(){
+		this.sub = + new Date()
+	}
+}
+</script>
---
+++ pages/user/user_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+	  Hello user {{$route.params.user_id}}
+  </div>	  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
