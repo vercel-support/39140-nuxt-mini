78727bfb4d577c656917f14ece2475e07d232b6a
Costantin Bignoli <cosbgn@gmail.com>
2021-08-19 13:11:47 +0200

    sub

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,11 @@
   <div>
 	  Homepage.
 	  <br>
-	  Visiting <a :href="`https://${sub}.loginhook.com`">{{sub}}.loginhook.com</a> should show the message: "Hello user {{sub}}"
+	   <br>
+	  Visiting <a :href="`https://${sub}.loginhook.com`">{{sub}}.loginhook.com</a> it should show the message: "Hello user {{sub}}"
+	  (The content of <a :href="`https://loginhook.com/user/${sub}`">https://loginhook.com/user/{{sub}}</a>)
 	  <br>
+	   <br>
 	  Instead it shows this page (The homepage)
   </div>
 </template>
